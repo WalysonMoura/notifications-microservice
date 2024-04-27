2be876284c2144d7aed7364889cedc9ad0d2e173
{ Walyson } <walysonmoura222@gmail.com>
2024-04-27 18:48:42 +0000

    new

--- src/infra/database/prisma/repositories/prisma-notifications-repository.ts
+++ src/infra/database/prisma/repositories/prisma-notifications-repository.ts
@@ -2,14 +2,18 @@ import { NotificationsRepository } from '@application/repositories/notifications
 import { PrismaService } from '../prisma.service';
 import { Notification } from '@application/entities/notification';
 import { Injectable } from '@nestjs/common';
+import { PrismaNotificationMapper } from '../mappers/prisma-notification-mapper';
 
 @Injectable()
 export class PrismaNotificationsrRepository implements NotificationsRepository {
   constructor(private prisma: PrismaService) {}
 
-  create(notification: Notification): Promise<void> {
-    throw new Error('Method not implemented.');
+  async create(notification: Notification): Promise<void> {
+    const raw = PrismaNotificationMapper.toPrisma(notification);
+
+    await this.prisma.notifications.create({ data: raw });
   }
+
   findById({ notificationId }: FindByIdParams): Promise<Notification | null> {
     throw new Error('Method not implemented.');
   }
