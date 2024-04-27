6d97c6181766e2d3e5c1b5afb3784f40c230f4c7
{ Walyson } <walysonmoura222@gmail.com>
2024-04-27 18:38:17 +0000

    new

--- src/infra/database/prisma/repositories/prisma-notifications-repository.ts
+++ src/infra/database/prisma/repositories/prisma-notifications-repository.ts
@@ -1,9 +1,29 @@
-import { PrismaService } from "../prisma.service";
+import { NotificationsRepository } from '@application/repositories/notifications-repository';
+import { PrismaService } from '../prisma.service';
+import { Notification } from '@application/entities/notification';
+import { Injectable } from '@nestjs/common';
 
+@Injectable()
+export class PrismaNotificationsrRepository implements NotificationsRepository {
+  constructor(private prisma: PrismaService) {}
 
-
-export class PrismaNotificationsrRepository {
-    constructor(private prisma:PrismaService) {
-        
-    }
+  create(notification: Notification): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+  findById({ notificationId }: FindByIdParams): Promise<Notification | null> {
+    throw new Error('Method not implemented.');
+  }
+  save(notification: Notification): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+  countManyByRecipientId({
+    recipientId,
+  }: CountManyByRecipientIdParams): Promise<number> {
+    throw new Error('Method not implemented.');
+  }
+  findManyByRecipientId({
+    recipientId,
+  }: FindManyByRecipientIdParams): Promise<Notification[]> {
+    throw new Error('Method not implemented.');
+  }
 }
