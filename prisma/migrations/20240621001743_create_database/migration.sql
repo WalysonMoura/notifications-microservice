f7ab5694212e35157d1ab5f791272db43e23ccf6
{ Walyson } <walysonmoura222@gmail.com>
2024-06-21 00:18:20 +0000

    new

---
+++ prisma/migrations/20240621001743_create_database/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "Notifications" (
+    "id" TEXT NOT NULL,
+    "recipientId" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "category" TEXT NOT NULL,
+    "readAt" TIMESTAMP(3),
+    "canceledAt" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Notifications_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "Notifications_recipientId_idx" ON "Notifications"("recipientId");
